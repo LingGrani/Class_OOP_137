26fb780a3f9f944b6bb148bc56ee3c7042ee7f09
LingGrani <luthfiangger2510@gmail.com>
2023-04-10 10:50:19 +0700

    add attribut sks. method input, public access

--- Class_OOP_137/Class_OOP_137.cpp
+++ Class_OOP_137/Class_OOP_137.cpp
@@ -17,6 +17,16 @@ class Matakuliah
 private:
 	string kode;
 	string namaM;
+	int sks;
+public: 
+	void input() {
+	cout << "Masukan Kode Matakuliah ";
+	cin >> kode;
+	cout << "Masukan Nama Matakuliah ";
+	cin >> namaM;
+	cout << "Jumlah SKS ";
+	cin >> sks;
+}
 
 };
 int main()
